436bfff1367dde4a919aa6154d6a32234a131703
Elaheh Gholizadeh <egholizadeh@tyro.com>
2018-09-14 10:11:18 +1000

    terraform plan

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -6,4 +6,4 @@ echo "+++ :terraform: Terraform Init"
 terraform init \
     -backend-config="bucket=terraform-tfstate-elaheh"
 echo "+++ :terraform: Terraform Apply"
-terraform apply -auto-approve
+terraform apply tfplan-test
